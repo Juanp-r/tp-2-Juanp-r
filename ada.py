50f1cc09884f338e23f5e52fd4c7feccf6e81acb
Juan Prieto <162722098+Juanp-r@users.noreply.github.com>
2024-04-05 10:51:04 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,12 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    name=first_name +" "+last_name
+    namelower=f"{(name.lower())}"
+    print(namelower)
+    nametitle=f"{(name.title())}"
+    print(nametitle)
+    nameupper=f"{(name.upper())}"
+    print(nameupper)
+    namestrip=" 	"+namelower
+    print(namestrip)
